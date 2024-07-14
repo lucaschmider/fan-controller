fac869bd6db4d704458a84f9ec3f7916847c42be
Luca Schmider <luca.schmider@outlook.com>
2024-07-14 17:59:52 +0200

    Create infrastructure to gather speed data

---
+++ TestData/FanSpeedRelation/Firmware/Firmware.ino
@@ -0,0 +1,67 @@
+#define TACHOMETER_PIN 2
+#define CONTROL_PIN 11
+
+volatile int speed = 0;
+volatile long lastTachometerTrigger = 0;
+
+void setup() {
+  // Initialize PWM at Timer1
+  TCCR1A = 0xA2;
+  TCCR1B = 0x19;
+  TCNT1H = 0x00;
+  TCNT1L = 0x00;
+
+  // Initialize PWM Pin
+  pinMode(CONTROL_PIN, OUTPUT);
+
+  // Initialize Tachometer
+  lastTachometerTrigger = millis();
+  pinMode(TACHOMETER_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(TACHOMETER_PIN), onRevolution, RISING);
+
+  // Initialize serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("clear");
+
+  for(int dutyCycle = 0; dutyCycle <= 100; dutyCycle++) {
+    setDutyCycle(dutyCycle);
+    for(int iteration = 0; iteration < 10; iteration++) {
+      delay(1000);
+      Serial.println("dutyCycle:" + String(dutyCycle) + ",rpm:" + String(speed));
+    }
+  }
+  
+  while(true){
+    Serial.println("done");
+    delay(1000);
+  }
+}
+
+void setDutyCycle(int dutyCycle) {
+  // icr and frecuency values are set to generate a 25 kHZ signal
+  int icr = 639;
+
+  ICR1H = icr >> 8;
+  ICR1L = icr & 0x00ff;
+
+  // Sets the duty cycle with the value entered in line 12
+  OCR1A = icr * (dutyCycle / 100.0);
+}
+
+void onRevolution() {
+  long now = millis();
+  // Multiply by two because the tachometer triggers twice per revolution
+  int cycleTime = (now - lastTachometerTrigger) * 2;
+  lastTachometerTrigger = now;
+
+  if (cycleTime <= 0)
+  {
+      return;
+  }
+
+  int frequency_Hz = 1000 / cycleTime;
+  speed = frequency_Hz * 60;
+}
--- TestData/FanSpeedRelation/get_data.py
+++ TestData/FanSpeedRelation/get_data.py
@@ -1,7 +1,59 @@
-from os import environ
+from os import environ, path
+from serial import Serial
+from regex import Regex
+from datetime import datetime, UTC
+from io import open
+import csv
+
+# Settings
+serialBusName = "/dev/cu.usbmodem1101"
+baudRate = 9600
+exportDestination = "data"
+
+# Exit when running in the docker container
 is_docker = environ.get("IS_DOCKER", "false") == "true"
 if (is_docker):
     print("As of now, this script does not work while running inside of docker. Please run it locally.")
     exit(-1)
 
-print("Listening for data on the serial bus.");
+# Begin data collection
+dataPattern = Regex(rb"dutyCycle:(\d+),rpm:(\d+)\s*")
+
+serialBus = Serial(serialBusName, baudRate)
+print("Serial bus opened. Sending the start signal");
+
+
+data = []
+while True:
+    currentLine = serialBus.readline().strip()
+
+    if currentLine == b"clear":
+        print("Registered the start of a new data set.")
+        data.clear()
+        continue
+
+    if currentLine == b"done":
+        break
+
+    matches = dataPattern.match(currentLine)
+    if matches is None:
+        print(f"Ignoring an invalid line: {currentLine}")
+        continue
+    
+    dutyCycle = int(matches.group(1))
+    rpm = int(matches.group(2))
+    data.append((dutyCycle, rpm))
+
+    print(f"dutyCycle: {dutyCycle}\trpm: {rpm}")
+serialBus.close()
+
+print(f"Measurement is done. {len(data)} records have been collected.")
+
+data.insert(0, ("dutyCycle", "rpm"))
+
+fileName = "measurement-" + datetime.now(UTC).strftime("%Y-%m-%d") + ".csv"
+with open(path.join(exportDestination, fileName), "w") as fileObject:
+    writer = csv.writer(fileObject)
+    writer.writerows(data)
+
+print("Done.");
